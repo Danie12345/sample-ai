e905529a270170576368cd02e29aca67e23be708
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-15 13:49:33 -0500

    Rename gen_data.py to functions.py

---
+++ src/functions.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+rg = np.random.default_rng()
+
+def gen_data(n_features, n_values):
+    features = rg.random((n_features, n_values))
+    weights = rg.random((1, n_values))[0]
+    targets = np.random.choice([0, 1], n_features)
+    data = pd.DataFrame(features, columns = ["x0", "x1", "x2"])
+    data["targets"] = targets
+    return data, weights
