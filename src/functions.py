daebb1172d53412bbc9d758aa45adba82ee2de61
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-15 14:31:51 -0500

    Add cross_entropy

--- src/functions.py
+++ src/functions.py
@@ -4,15 +4,18 @@ import pandas as pd
 rg = np.random.default_rng()
 
 def gen_data(n_features, n_values):
-    features = rg.random((n_features, n_values))
-    weights = rg.random((1, n_values))[0]
-    targets = np.random.choice([0, 1], n_features)
-    data = pd.DataFrame(features, columns = ["x0", "x1", "x2"])
-    data["targets"] = targets
-    return data, weights
+	features = rg.random((n_features, n_values))
+	weights = rg.random((1, n_values))[0]
+	targets = np.random.choice([0, 1], n_features)
+	data = pd.DataFrame(features, columns = ["x0", "x1", "x2"])
+	data["targets"] = targets
+	return data, weights
 
-def get_weighted_sum(feature, weights, bias=bias):
-  return np.dot(feature, weights) + bias
+def get_weighted_sum(feature, weights, bias):
+	return np.dot(feature, weights) + bias
 
 def sigmoid(w_sum):
-  return 1/(1 + np.exp(-w_sum))
+	return 1/(1 + np.exp(-w_sum))
+
+def cross_entropy(target, prediction):
+	return -(target * np.log10(prediction) + (1 + target) * np.log10(1 - prediction))
--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pandas as pd
-from functions import gen_data, get_weighted_sum, sigmoid
+from functions import gen_data, get_weighted_sum, sigmoid, cross_entropy
 
 bias = .5
 
 data, weights = gen_data(4, 3)
 
 for i in range(len(data)):
-  feature = data.loc[i][:-1]
-  target = data.loc[i][-1]
-  w_sum = get_weighted_sum(feature, weights, bias)
-  prediction = sigmoid(w_sum)
-  print (prediction)
+	feature = data.loc[i][:-1]
+	target = data.loc[i][-1]
+	w_sum = get_weighted_sum(feature, weights, bias)
+	prediction = sigmoid(w_sum)
+	loss = cross_entropy(target, prediction)
+	print(loss)
