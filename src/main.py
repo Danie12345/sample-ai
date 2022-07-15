9b968a7e6fe3f5f7c5df7e1d3fa7679ed967fc81
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-15 15:14:07 -0500

    Save train loss data
    Ignore saved training loss data

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ import pandas as pd
 from functions import gen_data, get_weighted_sum, sigmoid, cross_entropy, update_weights, update_bias
 
 bias = .5
-l_rate = .1
-epochs = 10
+l_rate = .001
+epochs = 20000
 epoch_loss = []
-data, weights = gen_data(4, 3)
+data, weights = gen_data(50, 3)
 
 def train_model(data, weights, bias, l_rate=l_rate, epochs=epochs):
 	for e in range(epochs):
@@ -23,8 +23,9 @@ def train_model(data, weights, bias, l_rate=l_rate, epochs=epochs):
 			bias = update_bias(bias, l_rate, target, prediction)
 		avg_loss = sum(individual_loss) / len(individual_loss)
 		epoch_loss.append(avg_loss)
-		print ("********************************")
-		print ("epochs", e)
-		print (avg_loss)
 
 train_model(data, weights, bias, l_rate, epochs)
+
+df = pd.DataFrame(epoch_loss)
+df_plot = df.plot(kind='line', grid=True).get_figure()
+df_plot.savefig('src/assets/training/training_loss.png')
