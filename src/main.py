2d833e07ebe62dd9aea4d8fe41a1c2b403f614d4
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-07-15 14:39:57 -0500

    Update weights

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
+from turtle import update
 import numpy as np
 import pandas as pd
-from functions import gen_data, get_weighted_sum, sigmoid, cross_entropy
+from functions import gen_data, get_weighted_sum, sigmoid, cross_entropy, update_weights
 
 bias = .5
 l_rate = .05
@@ -13,4 +14,4 @@ for i in range(len(data)):
 	w_sum = get_weighted_sum(feature, weights, bias)
 	prediction = sigmoid(w_sum)
 	loss = cross_entropy(target, prediction)
-	print(loss)
+	weights = update_weights(weights, l_rate, target, prediction)
